eaac8827e8c4cbf1c9bd94d0670b0297be54f27c
echobass <willem.brosz@gmail.com>
2015-02-10 16:08:58 -0800

    Better interface

---
+++ src/main/java/BattleSnakeHandlers.java
@@ -0,0 +1,32 @@
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class BattleSnakeHandlers {
+    
+    public Object handleStart(Map<String, Object> requestBody) {
+        // Dummy Response
+        Map<String, Object> responseObject = new HashMap<String, Object>();
+        responseObject.put("name", "battlesnake-java");
+        responseObject.put("color", "#ff0000");
+        responseObject.put("head_url", "http://battlesnake-java.herokuapp.com/");
+        responseObject.put("taunt", "battlesnake-java");
+        return responseObject;
+    }
+    
+    public Object handleMove(Map<String, Object> requestBody) {
+        // Dummy Response
+        Map<String, Object> responseObject = new HashMap<String, Object>();
+        responseObject.put("move", "up");
+        responseObject.put("taunt", "going up!");
+        return responseObject;
+    }
+    
+    public Object handleEnd(Map<String, Object> requestBody) {
+        // No response required
+        return null;
+    }
+
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,7 @@
 
 
 import java.io.IOException;
+import java.util.Map;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -11,10 +12,14 @@ import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 @SuppressWarnings("serial")
 public class Main extends HttpServlet {
     
-    private static Handlers HANDLERS = new Handlers();
+    private static ObjectMapper JSON_MAPPER = new ObjectMapper();
+    
+    private static BattleSnakeHandlers HANDLERS = new BattleSnakeHandlers();
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -24,16 +29,30 @@ public class Main extends HttpServlet {
     
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        System.out.println(String.format("Request: POST %s", req.getRequestURI()));
-        if (req.getRequestURI().endsWith("/start")) {
-            HANDLERS.handleStart(req, resp);
-        } else if (req.getRequestURI().endsWith("/move")) {
-            HANDLERS.handleMove(req, resp);
-        } else if (req.getRequestURI().endsWith("/end")) {
-            HANDLERS.handleEnd(req, resp);
+        String uri = req.getRequestURI();
+        Map<String, Object> requestObject = getRequestObject(req);
+        System.out.println(String.format("Request: POST %s %s", uri, requestObject));
+        
+        Object responseObject;
+        if (uri.endsWith("/start")) {
+            responseObject = HANDLERS.handleStart(requestObject);
+        } else if (uri.endsWith("/move")) {
+            responseObject = HANDLERS.handleMove(requestObject);
+        } else if (uri.endsWith("/end")) {
+            responseObject = HANDLERS.handleEnd(requestObject);
         } else {
             resp.sendError(HttpServletResponse.SC_NOT_FOUND);
+            return;
         }
+        
+        String responseJson = (responseObject != null) ? JSON_MAPPER.writeValueAsString(responseObject) : "{}";
+        resp.getWriter().print(responseJson);
+    }
+    
+    @SuppressWarnings("unchecked")
+    private Map<String, Object> getRequestObject(HttpServletRequest req) throws IOException {
+        Map<String, Object> requestObject = JSON_MAPPER.readValue(req.getReader(), Map.class);
+        return requestObject;
     }
 
     public static void main(String[] args) throws Exception {
