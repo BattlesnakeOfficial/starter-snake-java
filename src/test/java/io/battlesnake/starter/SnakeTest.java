f96c0183189b00e209736c7beabd7a0382daa6bb
codeallthethingz <howdoicontactwill@gmail.com>
2019-01-09 17:35:45 -0800

    upgraded jackson

--- src/test/java/io/battlesnake/starter/SnakeTest.java
+++ src/test/java/io/battlesnake/starter/SnakeTest.java
@@ -11,7 +11,6 @@ import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-
 public class SnakeTest {
 
     private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
