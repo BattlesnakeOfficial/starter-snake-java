10d7879db40ab41e93f4a9d56d81fe19ec711c20
Nettogrof <carl.lajeunesse@gmail.com>
2021-10-06 04:45:44 -0400

    Fix errors in the indexTest, and provide a real payload for the moveTest

--- src/test/java/com/battlesnake/starter/SnakeTest.java
+++ src/test/java/com/battlesnake/starter/SnakeTest.java
@@ -34,8 +34,8 @@ public class SnakeTest {
         
         Map<String, String> response = handler.index();
         assertEquals("#888888", response.get("color"));
-        assertEquals("default", response.get("headType"));
-        assertEquals("default", response.get("tailType"));
+        assertEquals("default", response.get("head"));
+        assertEquals("default", response.get("tail"));
     }
 
     @Test
@@ -49,7 +49,7 @@ public class SnakeTest {
 
     @Test
     void moveTest() throws IOException {
-        JsonNode moveRequest = OBJECT_MAPPER.readTree("{}");
+        JsonNode moveRequest = OBJECT_MAPPER.readTree("{\"game\":{\"id\":\"game-00fe20da-94ad-11ea-bb37\",\"ruleset\":{\"name\":\"standard\",\"version\":\"v.1.2.3\"},\"timeout\":500},\"turn\":14,\"board\":{\"height\":11,\"width\":11,\"food\":[{\"x\":5,\"y\":5},{\"x\":9,\"y\":0},{\"x\":2,\"y\":6}],\"hazards\":[{\"x\":3,\"y\":2}],\"snakes\":[{\"id\":\"snake-508e96ac-94ad-11ea-bb37\",\"name\":\"My Snake\",\"health\":54,\"body\":[{\"x\":0,\"y\":0},{\"x\":1,\"y\":0},{\"x\":2,\"y\":0}],\"latency\":\"111\",\"head\":{\"x\":0,\"y\":0},\"length\":3,\"shout\":\"why are we shouting??\",\"squad\":\"\"},{\"id\":\"snake-b67f4906-94ae-11ea-bb37\",\"name\":\"Another Snake\",\"health\":16,\"body\":[{\"x\":5,\"y\":4},{\"x\":5,\"y\":3},{\"x\":6,\"y\":3},{\"x\":6,\"y\":2}],\"latency\":\"222\",\"head\":{\"x\":5,\"y\":4},\"length\":4,\"shout\":\"I'm not really sure...\",\"squad\":\"\"}]},\"you\":{\"id\":\"snake-508e96ac-94ad-11ea-bb37\",\"name\":\"My Snake\",\"health\":54,\"body\":[{\"x\":0,\"y\":0},{\"x\":1,\"y\":0},{\"x\":2,\"y\":0}],\"latency\":\"111\",\"head\":{\"x\":0,\"y\":0},\"length\":3,\"shout\":\"why are we shouting??\",\"squad\":\"\"}}");
         Map<String, String> response = handler.move(moveRequest);
 
         List<String> options = new ArrayList<String>();
