f43206d6bf17b1834437d809d057f49dc98ac709
Nettogrof <carl.lajeunesse@gmail.com>
2021-10-06 04:17:37 -0400

    AvoidNeck + comments
    Added a function to avoidMyNeck to move selection. And added TODO comments to let new developers knows next steps to do.

--- src/main/java/com/battlesnake/starter/Snake.java
+++ src/main/java/com/battlesnake/starter/Snake.java
@@ -8,6 +8,8 @@ import org.slf4j.LoggerFactory;
 import spark.Request;
 import spark.Response;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
@@ -23,158 +25,225 @@ import static spark.Spark.get;
  * https://github.com/BattlesnakeOfficial/starter-snake-java/README.md
  */
 public class Snake {
-    private static final ObjectMapper JSON_MAPPER = new ObjectMapper();
-    private static final Handler HANDLER = new Handler();
-    private static final Logger LOG = LoggerFactory.getLogger(Snake.class);
+	private static final ObjectMapper JSON_MAPPER = new ObjectMapper();
+	private static final Handler HANDLER = new Handler();
+	private static final Logger LOG = LoggerFactory.getLogger(Snake.class);
 
-    /**
-     * Main entry point.
-     *
-     * @param args are ignored.
-     */
-    public static void main(String[] args) {
-        String port = System.getProperty("PORT");
-        if (port != null) {
-            LOG.info("Found system provided port: {}", port);
-        } else {
-            LOG.info("Using default port: {}", port);
-            port = "8080";
-        }
-        port(Integer.parseInt(port));
-        get("/",  HANDLER::process, JSON_MAPPER::writeValueAsString);
-        post("/start", HANDLER::process, JSON_MAPPER::writeValueAsString);
-        post("/move", HANDLER::process, JSON_MAPPER::writeValueAsString);
-        post("/end", HANDLER::process, JSON_MAPPER::writeValueAsString);
-    }
+	/**
+	 * Main entry point.
+	 *
+	 * @param args are ignored.
+	 */
+	public static void main(String[] args) {
+		String port = System.getProperty("PORT");
+		if (port == null) {
+			LOG.info("Using default port: {}", port);
+			port = "8080";
+		} else {
+			LOG.info("Found system provided port: {}", port);			
+		}
+		port(Integer.parseInt(port));
+		get("/", HANDLER::process, JSON_MAPPER::writeValueAsString);
+		post("/start", HANDLER::process, JSON_MAPPER::writeValueAsString);
+		post("/move", HANDLER::process, JSON_MAPPER::writeValueAsString);
+		post("/end", HANDLER::process, JSON_MAPPER::writeValueAsString);
+	}
 
-    /**
-     * Handler class for dealing with the routes set up in the main method.
-     */
-    public static class Handler {
+	/**
+	 * Handler class for dealing with the routes set up in the main method.
+	 */
+	public static class Handler {
 
-        /**
-         * For the start/end request
-         */
-        private static final Map<String, String> EMPTY = new HashMap<>();
+		/**
+		 * For the start/end request
+		 */
+		private static final Map<String, String> EMPTY = new HashMap<>();
 
-        /**
-         * Generic processor that prints out the request and response from the methods.
-         *
-         * @param req
-         * @param res
-         * @return
-         */
-        public Map<String, String> process(Request req, Response res) {
-            try {
-                JsonNode parsedRequest = JSON_MAPPER.readTree(req.body());
-                String uri = req.uri();
-                LOG.info("{} called with: {}", uri, req.body());
-                Map<String, String> snakeResponse;
-                if (uri.equals("/")) {
-                    snakeResponse = index();
-                } else if (uri.equals("/start")) {
-                    snakeResponse = start(parsedRequest);
-                } else if (uri.equals("/move")) {
-                    snakeResponse = move(parsedRequest);
-                } else if (uri.equals("/end")) {
-                    snakeResponse = end(parsedRequest);
-                } else {
-                    throw new IllegalAccessError("Strange call made to the snake: " + uri);
-                }
-                LOG.info("Responding with: {}", JSON_MAPPER.writeValueAsString(snakeResponse));
-                return snakeResponse;
-            } catch (Exception e) {
-                LOG.warn("Something went wrong!", e);
-                return null;
-            }
-        }
+		/**
+		 * Generic processor that prints out the request and response from the methods.
+		 *
+		 * @param req
+		 * @param res
+		 * @return
+		 */
+		public Map<String, String> process(Request req, Response res) {
+			try {
+				JsonNode parsedRequest = JSON_MAPPER.readTree(req.body());
+				String uri = req.uri();
+				LOG.info("{} called with: {}", uri, req.body());
+				Map<String, String> snakeResponse;
+				if (uri.equals("/")) {
+					snakeResponse = index();
+				} else if (uri.equals("/start")) {
+					snakeResponse = start(parsedRequest);
+				} else if (uri.equals("/move")) {
+					snakeResponse = move(parsedRequest);
+				} else if (uri.equals("/end")) {
+					snakeResponse = end(parsedRequest);
+				} else {
+					throw new IllegalAccessError("Strange call made to the snake: " + uri);
+				}
 
-    
-        /**
-         * This method is called everytime your Battlesnake is entered into a game.
-         * 
-         * Use this method to decide how your Battlesnake is going to look on the board.
-         *
-         * @return a response back to the engine containing the Battlesnake setup
-         *         values.
-         */
-        public Map<String, String> index() {         
-            Map<String, String> response = new HashMap<>();
-            response.put("apiversion", "1");
-            response.put("author", "");           // TODO: Your Battlesnake Username
-            response.put("color", "#888888");     // TODO: Personalize
-            response.put("head", "default");  // TODO: Personalize
-            response.put("tail", "default");  // TODO: Personalize
-            return response;
-        }
+				LOG.info("Responding with: {}", JSON_MAPPER.writeValueAsString(snakeResponse));
 
-        /**
-         * This method is called everytime your Battlesnake is entered into a game.
-         * 
-         * Use this method to decide how your Battlesnake is going to look on the board.
-         *
-         * @param startRequest a JSON data map containing the information about the game
-         *                     that is about to be played.
-         * @return responses back to the engine are ignored.
-         */
-        public Map<String, String> start(JsonNode startRequest) {
-            LOG.info("START");
-            return EMPTY;
-        }
+				return snakeResponse;
+			} catch (JsonProcessingException e) {
+				LOG.warn("Something went wrong!", e);
+				return null;
+			}
+		}
 
-        /**
-         * This method is called on every turn of a game. It's how your snake decides
-         * where to move.
-         * 
-         * Valid moves are "up", "down", "left", or "right".
-         *
-         * @param moveRequest a map containing the JSON sent to this snake. Use this
-         *                    data to decide your next move.
-         * @return a response back to the engine containing Battlesnake movement values.
-         */
-        public Map<String, String> move(JsonNode moveRequest) {
-            try {
-                LOG.info("Data: {}", JSON_MAPPER.writerWithDefaultPrettyPrinter().writeValueAsString(moveRequest));
-            } catch (JsonProcessingException e) {
-                e.printStackTrace();
-            }
+		/**
+		 * This method is called everytime your Battlesnake is entered into a game.
+		 * 
+		 * Use this method to decide how your Battlesnake is going to look on the board.
+		 *
+		 * @return a response back to the engine containing the Battlesnake setup
+		 *         values.
+		 */
+		public Map<String, String> index() {
+			Map<String, String> response = new HashMap<>();
+			response.put("apiversion", "1");
+			response.put("author", ""); // TODO: Your Battlesnake Username
+			response.put("color", "#888888"); // TODO: Personalize
+			response.put("head", "default"); // TODO: Personalize
+			response.put("tail", "default"); // TODO: Personalize
+			return response;
+		}
 
-            /*
-                Example how to retrieve data from the request payload:
+		/**
+		 * This method is called everytime your Battlesnake is entered into a game.
+		 * 
+		 * Use this method to decide how your Battlesnake is going to look on the board.
+		 *
+		 * @param startRequest a JSON data map containing the information about the game
+		 *                     that is about to be played.
+		 * @return responses back to the engine are ignored.
+		 */
+		public Map<String, String> start(JsonNode startRequest) {
+			LOG.info("START");
+			return EMPTY;
+		}
 
-                String gameId = moveRequest.get("game").get("id").asText();
-                int height = moveRequest.get("board").get("height").asInt();
+		/**
+		 * This method is called on every turn of a game. It's how your snake decides
+		 * where to move.
+		 * 
+		 * Valid moves are "up", "down", "left", or "right".
+		 *
+		 * @param moveRequest a map containing the JSON sent to this snake. Use this
+		 *                    data to decide your next move.
+		 * @return a response back to the engine containing Battlesnake movement values.
+		 */
+		public Map<String, String> move(JsonNode moveRequest) {
 
-            */
+			/*
+			 * moveRequest: JsonNoe of all Game Board data as received from the Battlesnake
+			 * Engine. For a full example of 'json', see
+			 * https://docs.battlesnake.com/references/api/sample-move-request return: A
+			 * String, the single move to make. One of "up", "down", "left" or "right". Use
+			 * the information in 'moveRequest' to decide your next move. The 'moveRequest'
+			 * variable can be interacted with as com.fasterxml.jackson.databind.JsonNode,
+			 * and contains all of the information about the Battlesnake board for each move
+			 * of the game.
+			 */
+			try {
+				LOG.info("Data: {}", JSON_MAPPER.writerWithDefaultPrettyPrinter().writeValueAsString(moveRequest));
+			} catch (JsonProcessingException e) {
+				LOG.error("Error parsing payload", e);
+			}
+			
+			JsonNode head = moveRequest.get("you").get("head");
+			JsonNode body = moveRequest.get("you").get("body");
 
-            String[] possibleMoves = { "up", "down", "left", "right" };
+			/*
+			 * Example how to retrieve data from the request payload:
+			 * 
+			 * String gameId = moveRequest.get("game").get("id").asText();
+			 * 
+			 * int height = moveRequest.get("board").get("height").asInt();
+			 * 
+			 */
 
-            // Choose a random direction to move in
-            int choice = new Random().nextInt(possibleMoves.length);
-            String move = possibleMoves[choice];
+			ArrayList<String> possibleMoves = new ArrayList<>(Arrays.asList("up", "down", "left", "right"));
 
-            LOG.info("MOVE {}", move);
+			// Don't allow your Battlesnake to move back in on it's own neck
+			
+			avoidMyNeck(head,body,possibleMoves);
+			
+			
+			/*
+			 * TODO: Using information from 'data', find the edges of the board and don't
+			 * let your Battlesnake move beyond them board_height = ? board_width = ?
+			 */
 
-            Map<String, String> response = new HashMap<>();
-            response.put("move", move);
-            return response;
-        }
+			// TODO Using information from 'data', don't let your Battlesnake pick a move
+			// that would hit its own body
 
-        /**
-         * This method is called when a game your Battlesnake was in ends.
-         * 
-         * It is purely for informational purposes, you don't have to make any decisions
-         * here.
-         *
-         * @param endRequest a map containing the JSON sent to this snake. Use this data
-         *                   to know which game has ended
-         * @return responses back to the engine are ignored.
-         */
-        public Map<String, String> end(JsonNode endRequest) {
+			// TODO: Using information from 'data', don't let your Battlesnake pick a move
+			// that would collide with another Battlesnake
 
-            LOG.info("END");
-            return EMPTY;
-        }
-    }
+			// TODO: Using information from 'data', make your Battlesnake move towards a
+			// piece of food on the board
+
+			// Choose a random direction to move in
+			final int choice = new Random().nextInt(possibleMoves.size());
+			final String move = possibleMoves.get(choice);
+
+			LOG.info("MOVE {}", move);
+
+			Map<String, String> response = new HashMap<>();
+			response.put("move", move);
+			return response;
+		}
+
+		/**
+		 * @param head
+		 * @param body
+		 * @param possibleMoves
+		 */
+		private void avoidMyNeck(JsonNode head, JsonNode body, ArrayList<String> possibleMoves) {
+			JsonNode neck = body.get(1);
+			
+			if (neck.get("x").asInt() < head.get("x").asInt()) {
+				possibleMoves.remove("left");
+			}else if(neck.get("x").asInt() > head.get("x").asInt()) {
+				possibleMoves.remove("right");
+			}else if(neck.get("y").asInt() < head.get("y").asInt()) {
+				possibleMoves.remove("down");
+			}else if(neck.get("y").asInt() > head.get("y").asInt()) {
+				possibleMoves.remove("up");
+			}
+			
+			/*
+			 * if my_neck["x"] < my_head["x"]:  # my neck is left of my head
+        possible_moves.remove("left")
+    elif my_neck["x"] > my_head["x"]:  # my neck is right of my head
+        possible_moves.remove("right")
+    elif my_neck["y"] < my_head["y"]:  # my neck is below my head
+        possible_moves.remove("down")
+    elif my_neck["y"] > my_head["y"]:  # my neck is above my head
+        possible_moves.remove("up")
+			 * 
+			 */
+			
+		}
+
+		/**
+		 * This method is called when a game your Battlesnake was in ends.
+		 * 
+		 * It is purely for informational purposes, you don't have to make any decisions
+		 * here.
+		 *
+		 * @param endRequest a map containing the JSON sent to this snake. Use this data
+		 *                   to know which game has ended
+		 * @return responses back to the engine are ignored.
+		 */
+		public Map<String, String> end(JsonNode endRequest) {
+
+			LOG.info("END");
+			return EMPTY;
+		}
+	}
 
 }
