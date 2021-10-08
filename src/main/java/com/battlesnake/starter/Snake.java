df12c2796375a7b1e209645b7b19a9b264b963da
Nettogrof <carl.lajeunesse@gmail.com>
2021-10-08 04:56:43 -0400

    Changing Tabs to space identation

--- src/main/java/com/battlesnake/starter/Snake.java
+++ src/main/java/com/battlesnake/starter/Snake.java
@@ -25,208 +25,212 @@ import static spark.Spark.get;
  * https://github.com/BattlesnakeOfficial/starter-snake-java/README.md
  */
 public class Snake {
-	private static final ObjectMapper JSON_MAPPER = new ObjectMapper();
-	private static final Handler HANDLER = new Handler();
-	private static final Logger LOG = LoggerFactory.getLogger(Snake.class);
+    private static final ObjectMapper JSON_MAPPER = new ObjectMapper();
+    private static final Handler HANDLER = new Handler();
+    private static final Logger LOG = LoggerFactory.getLogger(Snake.class);
 
-	/**
-	 * Main entry point.
-	 *
-	 * @param args are ignored.
-	 */
-	public static void main(String[] args) {
-		String port = System.getProperty("PORT");
-		if (port == null) {
-			LOG.info("Using default port: {}", port);
-			port = "8080";
-		} else {
-			LOG.info("Found system provided port: {}", port);
-		}
-		port(Integer.parseInt(port));
-		get("/", HANDLER::process, JSON_MAPPER::writeValueAsString);
-		post("/start", HANDLER::process, JSON_MAPPER::writeValueAsString);
-		post("/move", HANDLER::process, JSON_MAPPER::writeValueAsString);
-		post("/end", HANDLER::process, JSON_MAPPER::writeValueAsString);
-	}
+    /**
+     * Main entry point.
+     *
+     * @param args are ignored.
+     */
+    public static void main(String[] args) {
+        String port = System.getProperty("PORT");
+        if (port == null) {
+            LOG.info("Using default port: {}", port);
+            port = "8080";
+        } else {
+            LOG.info("Found system provided port: {}", port);
+        }
+        port(Integer.parseInt(port));
+        get("/", HANDLER::process, JSON_MAPPER::writeValueAsString);
+        post("/start", HANDLER::process, JSON_MAPPER::writeValueAsString);
+        post("/move", HANDLER::process, JSON_MAPPER::writeValueAsString);
+        post("/end", HANDLER::process, JSON_MAPPER::writeValueAsString);
+    }
 
-	/**
-	 * Handler class for dealing with the routes set up in the main method.
-	 */
-	public static class Handler {
+    /**
+     * Handler class for dealing with the routes set up in the main method.
+     */
+    public static class Handler {
 
-		/**
-		 * For the start/end request
-		 */
-		private static final Map<String, String> EMPTY = new HashMap<>();
+        /**
+         * For the start/end request
+         */
+        private static final Map<String, String> EMPTY = new HashMap<>();
 
-		/**
-		 * Generic processor that prints out the request and response from the methods.
-		 *
-		 * @param req
-		 * @param res
-		 * @return
-		 */
-		public Map<String, String> process(Request req, Response res) {
-			try {
-				JsonNode parsedRequest = JSON_MAPPER.readTree(req.body());
-				String uri = req.uri();
-				LOG.info("{} called with: {}", uri, req.body());
-				Map<String, String> snakeResponse;
-				if (uri.equals("/")) {
-					snakeResponse = index();
-				} else if (uri.equals("/start")) {
-					snakeResponse = start(parsedRequest);
-				} else if (uri.equals("/move")) {
-					snakeResponse = move(parsedRequest);
-				} else if (uri.equals("/end")) {
-					snakeResponse = end(parsedRequest);
-				} else {
-					throw new IllegalAccessError("Strange call made to the snake: " + uri);
-				}
+        /**
+         * Generic processor that prints out the request and response from the methods.
+         *
+         * @param req
+         * @param res
+         * @return
+         */
+        public Map<String, String> process(Request req, Response res) {
+            try {
+                JsonNode parsedRequest = JSON_MAPPER.readTree(req.body());
+                String uri = req.uri();
+                LOG.info("{} called with: {}", uri, req.body());
+                Map<String, String> snakeResponse;
+                if (uri.equals("/")) {
+                    snakeResponse = index();
+                } else if (uri.equals("/start")) {
+                    snakeResponse = start(parsedRequest);
+                } else if (uri.equals("/move")) {
+                    snakeResponse = move(parsedRequest);
+                } else if (uri.equals("/end")) {
+                    snakeResponse = end(parsedRequest);
+                } else {
+                    throw new IllegalAccessError("Strange call made to the snake: " + uri);
+                }
 
-				LOG.info("Responding with: {}", JSON_MAPPER.writeValueAsString(snakeResponse));
+                LOG.info("Responding with: {}", JSON_MAPPER.writeValueAsString(snakeResponse));
 
-				return snakeResponse;
-			} catch (JsonProcessingException e) {
-				LOG.warn("Something went wrong!", e);
-				return null;
-			}
-		}
+                return snakeResponse;
+            } catch (JsonProcessingException e) {
+                LOG.warn("Something went wrong!", e);
+                return null;
+            }
+        }
 
-		/**
-		 * This method is called everytime your Battlesnake is entered into a game.
-		 * 
-		 * Use this method to decide how your Battlesnake is going to look on the board.
-		 *
-		 * @return a response back to the engine containing the Battlesnake setup
-		 *         values.
-		 */
-		public Map<String, String> index() {
-			Map<String, String> response = new HashMap<>();
-			response.put("apiversion", "1");
-			response.put("author", ""); // TODO: Your Battlesnake Username
-			response.put("color", "#888888"); // TODO: Personalize
-			response.put("head", "default"); // TODO: Personalize
-			response.put("tail", "default"); // TODO: Personalize
-			return response;
-		}
+        /**
+         * This method is called everytime your Battlesnake is entered into a game.
+         * 
+         * Use this method to decide how your Battlesnake is going to look on the board.
+         *
+         * @return a response back to the engine containing the Battlesnake setup
+         *         values.
+         */
+        public Map<String, String> index() {
+            Map<String, String> response = new HashMap<>();
+            response.put("apiversion", "1");
+            response.put("author", ""); // TODO: Your Battlesnake Username
+            response.put("color", "#888888"); // TODO: Personalize
+            response.put("head", "default"); // TODO: Personalize
+            response.put("tail", "default"); // TODO: Personalize
+            return response;
+        }
 
-		/**
-		 * This method is called everytime your Battlesnake is entered into a game.
-		 * 
-		 * Use this method to decide how your Battlesnake is going to look on the board.
-		 *
-		 * @param startRequest a JSON data map containing the information about the game
-		 *                     that is about to be played.
-		 * @return responses back to the engine are ignored.
-		 */
-		public Map<String, String> start(JsonNode startRequest) {
-			LOG.info("START");
-			return EMPTY;
-		}
+        /**
+         * This method is called everytime your Battlesnake is entered into a game.
+         * 
+         * Use this method to decide how your Battlesnake is going to look on the board.
+         *
+         * @param startRequest a JSON data map containing the information about the game
+         *                     that is about to be played.
+         * @return responses back to the engine are ignored.
+         */
+        public Map<String, String> start(JsonNode startRequest) {
+            LOG.info("START");
+            return EMPTY;
+        }
 
-		/**
-		 * This method is called on every turn of a game. It's how your snake decides
-		 * where to move.
-		 * 
-		 * Use the information in 'moveRequest' to decide your next move. The
-		 * 'moveRequest' variable can be interacted with as
-		 * com.fasterxml.jackson.databind.JsonNode, and contains all of the information
-		 * about the Battlesnake board for each move of the game.
-		 * 
-		 * For a full example of 'json', see
-		 * https://docs.battlesnake.com/references/api/sample-move-request
-		 *
-		 * @param moveRequest JsonNode of all Game Board data as received from the
-		 *                    Battlesnake Engine.
-		 * @return a Map<String,String> response back to the engine the single move to
-		 *         make. One of "up", "down", "left" or "right".
-		 */
-		public Map<String, String> move(JsonNode moveRequest) {
+        /**
+         * This method is called on every turn of a game. It's how your snake decides
+         * where to move.
+         * 
+         * Use the information in 'moveRequest' to decide your next move. The
+         * 'moveRequest' variable can be interacted with as
+         * com.fasterxml.jackson.databind.JsonNode, and contains all of the information
+         * about the Battlesnake board for each move of the game.
+         * 
+         * For a full example of 'json', see
+         * https://docs.battlesnake.com/references/api/sample-move-request
+         *
+         * @param moveRequest JsonNode of all Game Board data as received from the
+         *                    Battlesnake Engine.
+         * @return a Map<String,String> response back to the engine the single move to
+         *         make. One of "up", "down", "left" or "right".
+         */
+        public Map<String, String> move(JsonNode moveRequest) {
 
-			try {
-				LOG.info("Data: {}", JSON_MAPPER.writerWithDefaultPrettyPrinter().writeValueAsString(moveRequest));
-			} catch (JsonProcessingException e) {
-				LOG.error("Error parsing payload", e);
-			}
-			
-			/*
-			 * Example how to retrieve data from the request payload:
-			 * 
-			 * String gameId = moveRequest.get("game").get("id").asText();
-			 * 
-			 * int height = moveRequest.get("board").get("height").asInt();
-			 * 
-			 */
-			
-			JsonNode head = moveRequest.get("you").get("head");
-			JsonNode body = moveRequest.get("you").get("body");
-			
-			ArrayList<String> possibleMoves = new ArrayList<>(Arrays.asList("up", "down", "left", "right"));
+            try {
+                LOG.info("Data: {}", JSON_MAPPER.writerWithDefaultPrettyPrinter().writeValueAsString(moveRequest));
+            } catch (JsonProcessingException e) {
+                LOG.error("Error parsing payload", e);
+            }
 
-			// Don't allow your Battlesnake to move back in on it's own neck
-			avoidMyNeck(head, body, possibleMoves);
+            /*
+             * Example how to retrieve data from the request payload:
+             * 
+             * String gameId = moveRequest.get("game").get("id").asText();
+             * 
+             * int height = moveRequest.get("board").get("height").asInt();
+             * 
+             */
 
-			// TODO: Using information from 'moveRequest', find the edges of the board and don't
-			// let your Battlesnake move beyond them board_height = ? board_width = ?
+            JsonNode head = moveRequest.get("you").get("head");
+            JsonNode body = moveRequest.get("you").get("body");
 
-			// TODO Using information from 'moveRequest', don't let your Battlesnake pick a move
-			// that would hit its own body
+            ArrayList<String> possibleMoves = new ArrayList<>(Arrays.asList("up", "down", "left", "right"));
 
-			// TODO: Using information from 'moveRequest', don't let your Battlesnake pick a move
-			// that would collide with another Battlesnake
+            // Don't allow your Battlesnake to move back in on it's own neck
+            avoidMyNeck(head, body, possibleMoves);
 
-			// TODO: Using information from 'moveRequest', make your Battlesnake move towards a
-			// piece of food on the board
+            // TODO: Using information from 'moveRequest', find the edges of the board and
+            // don't
+            // let your Battlesnake move beyond them board_height = ? board_width = ?
 
-			// Choose a random direction to move in
-			final int choice = new Random().nextInt(possibleMoves.size());
-			final String move = possibleMoves.get(choice);
+            // TODO Using information from 'moveRequest', don't let your Battlesnake pick a
+            // move
+            // that would hit its own body
 
-			LOG.info("MOVE {}", move);
+            // TODO: Using information from 'moveRequest', don't let your Battlesnake pick a
+            // move
+            // that would collide with another Battlesnake
 
-			Map<String, String> response = new HashMap<>();
-			response.put("move", move);
-			return response;
-		}
+            // TODO: Using information from 'moveRequest', make your Battlesnake move
+            // towards a
+            // piece of food on the board
 
-		/**
-		 * Remove the 'neck' direction from the list of possible moves
-		 * 
-		 * @param head          JsonNode of the head position e.g. {"x": 0, "y": 0}
-		 * @param body          JsonNode of x/y coordinates for every segment of a
-		 *                      Battlesnake. e.g. [ {"x": 0, "y": 0}, {"x": 1, "y": 0},
-		 *                      {"x": 2, "y": 0} ]
-		 * @param possibleMoves ArrayList of String. Moves to pick from.
-		 */
-		public void avoidMyNeck(JsonNode head, JsonNode body, ArrayList<String> possibleMoves) {
-			JsonNode neck = body.get(1);
+            // Choose a random direction to move in
+            final int choice = new Random().nextInt(possibleMoves.size());
+            final String move = possibleMoves.get(choice);
 
-			if (neck.get("x").asInt() < head.get("x").asInt()) {
-				possibleMoves.remove("left");
-			} else if (neck.get("x").asInt() > head.get("x").asInt()) {
-				possibleMoves.remove("right");
-			} else if (neck.get("y").asInt() < head.get("y").asInt()) {
-				possibleMoves.remove("down");
-			} else if (neck.get("y").asInt() > head.get("y").asInt()) {
-				possibleMoves.remove("up");
-			}
-		}
+            LOG.info("MOVE {}", move);
 
-		/**
-		 * This method is called when a game your Battlesnake was in ends.
-		 * 
-		 * It is purely for informational purposes, you don't have to make any decisions
-		 * here.
-		 *
-		 * @param endRequest a map containing the JSON sent to this snake. Use this data
-		 *                   to know which game has ended
-		 * @return responses back to the engine are ignored.
-		 */
-		public Map<String, String> end(JsonNode endRequest) {
-			LOG.info("END");
-			return EMPTY;
-		}
-	}
+            Map<String, String> response = new HashMap<>();
+            response.put("move", move);
+            return response;
+        }
+
+        /**
+         * Remove the 'neck' direction from the list of possible moves
+         * 
+         * @param head          JsonNode of the head position e.g. {"x": 0, "y": 0}
+         * @param body          JsonNode of x/y coordinates for every segment of a
+         *                      Battlesnake. e.g. [ {"x": 0, "y": 0}, {"x": 1, "y": 0},
+         *                      {"x": 2, "y": 0} ]
+         * @param possibleMoves ArrayList of String. Moves to pick from.
+         */
+        public void avoidMyNeck(JsonNode head, JsonNode body, ArrayList<String> possibleMoves) {
+            JsonNode neck = body.get(1);
+
+            if (neck.get("x").asInt() < head.get("x").asInt()) {
+                possibleMoves.remove("left");
+            } else if (neck.get("x").asInt() > head.get("x").asInt()) {
+                possibleMoves.remove("right");
+            } else if (neck.get("y").asInt() < head.get("y").asInt()) {
+                possibleMoves.remove("down");
+            } else if (neck.get("y").asInt() > head.get("y").asInt()) {
+                possibleMoves.remove("up");
+            }
+        }
+
+        /**
+         * This method is called when a game your Battlesnake was in ends.
+         * 
+         * It is purely for informational purposes, you don't have to make any decisions
+         * here.
+         *
+         * @param endRequest a map containing the JSON sent to this snake. Use this data
+         *                   to know which game has ended
+         * @return responses back to the engine are ignored.
+         */
+        public Map<String, String> end(JsonNode endRequest) {
+            LOG.info("END");
+            return EMPTY;
+        }
+    }
 
 }
--- src/test/java/com/battlesnake/starter/SnakeTest.java
+++ src/test/java/com/battlesnake/starter/SnakeTest.java
@@ -17,129 +17,129 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class SnakeTest {
 
-	private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
-	static {
-		OBJECT_MAPPER.configure(JsonParser.Feature.ALLOW_SINGLE_QUOTES, true);
-	}
+    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+    static {
+        OBJECT_MAPPER.configure(JsonParser.Feature.ALLOW_SINGLE_QUOTES, true);
+    }
 
-	private Snake.Handler handler;
+    private Snake.Handler handler;
 
-	@BeforeEach
-	void setUp() {
-		handler = new Snake.Handler();
-	}
+    @BeforeEach
+    void setUp() {
+        handler = new Snake.Handler();
+    }
 
-	@Test
-	void indexTest() throws IOException {
+    @Test
+    void indexTest() throws IOException {
 
-		Map<String, String> response = handler.index();
-		assertEquals("#888888", response.get("color"));
-		assertEquals("default", response.get("head"));
-		assertEquals("default", response.get("tail"));
-	}
+        Map<String, String> response = handler.index();
+        assertEquals("#888888", response.get("color"));
+        assertEquals("default", response.get("head"));
+        assertEquals("default", response.get("tail"));
+    }
 
-	@Test
-	void startTest() throws IOException {
-		JsonNode startRequest = OBJECT_MAPPER.readTree("{}");
-		Map<String, String> response = handler.end(startRequest);
-		assertEquals(0, response.size());
+    @Test
+    void startTest() throws IOException {
+        JsonNode startRequest = OBJECT_MAPPER.readTree("{}");
+        Map<String, String> response = handler.end(startRequest);
+        assertEquals(0, response.size());
 
-	}
+    }
 
-	@Test
-	void moveTest() throws IOException {
-		JsonNode moveRequest = OBJECT_MAPPER.readTree(
-				"{\"game\":{\"id\":\"game-00fe20da-94ad-11ea-bb37\",\"ruleset\":{\"name\":\"standard\",\"version\":\"v.1.2.3\"},\"timeout\":500},\"turn\":14,\"board\":{\"height\":11,\"width\":11,\"food\":[{\"x\":5,\"y\":5},{\"x\":9,\"y\":0},{\"x\":2,\"y\":6}],\"hazards\":[{\"x\":3,\"y\":2}],\"snakes\":[{\"id\":\"snake-508e96ac-94ad-11ea-bb37\",\"name\":\"My Snake\",\"health\":54,\"body\":[{\"x\":0,\"y\":0},{\"x\":1,\"y\":0},{\"x\":2,\"y\":0}],\"latency\":\"111\",\"head\":{\"x\":0,\"y\":0},\"length\":3,\"shout\":\"why are we shouting??\",\"squad\":\"\"},{\"id\":\"snake-b67f4906-94ae-11ea-bb37\",\"name\":\"Another Snake\",\"health\":16,\"body\":[{\"x\":5,\"y\":4},{\"x\":5,\"y\":3},{\"x\":6,\"y\":3},{\"x\":6,\"y\":2}],\"latency\":\"222\",\"head\":{\"x\":5,\"y\":4},\"length\":4,\"shout\":\"I'm not really sure...\",\"squad\":\"\"}]},\"you\":{\"id\":\"snake-508e96ac-94ad-11ea-bb37\",\"name\":\"My Snake\",\"health\":54,\"body\":[{\"x\":0,\"y\":0},{\"x\":1,\"y\":0},{\"x\":2,\"y\":0}],\"latency\":\"111\",\"head\":{\"x\":0,\"y\":0},\"length\":3,\"shout\":\"why are we shouting??\",\"squad\":\"\"}}");
-		Map<String, String> response = handler.move(moveRequest);
+    @Test
+    void moveTest() throws IOException {
+        JsonNode moveRequest = OBJECT_MAPPER.readTree(
+                "{\"game\":{\"id\":\"game-00fe20da-94ad-11ea-bb37\",\"ruleset\":{\"name\":\"standard\",\"version\":\"v.1.2.3\"},\"timeout\":500},\"turn\":14,\"board\":{\"height\":11,\"width\":11,\"food\":[{\"x\":5,\"y\":5},{\"x\":9,\"y\":0},{\"x\":2,\"y\":6}],\"hazards\":[{\"x\":3,\"y\":2}],\"snakes\":[{\"id\":\"snake-508e96ac-94ad-11ea-bb37\",\"name\":\"My Snake\",\"health\":54,\"body\":[{\"x\":0,\"y\":0},{\"x\":1,\"y\":0},{\"x\":2,\"y\":0}],\"latency\":\"111\",\"head\":{\"x\":0,\"y\":0},\"length\":3,\"shout\":\"why are we shouting??\",\"squad\":\"\"},{\"id\":\"snake-b67f4906-94ae-11ea-bb37\",\"name\":\"Another Snake\",\"health\":16,\"body\":[{\"x\":5,\"y\":4},{\"x\":5,\"y\":3},{\"x\":6,\"y\":3},{\"x\":6,\"y\":2}],\"latency\":\"222\",\"head\":{\"x\":5,\"y\":4},\"length\":4,\"shout\":\"I'm not really sure...\",\"squad\":\"\"}]},\"you\":{\"id\":\"snake-508e96ac-94ad-11ea-bb37\",\"name\":\"My Snake\",\"health\":54,\"body\":[{\"x\":0,\"y\":0},{\"x\":1,\"y\":0},{\"x\":2,\"y\":0}],\"latency\":\"111\",\"head\":{\"x\":0,\"y\":0},\"length\":3,\"shout\":\"why are we shouting??\",\"squad\":\"\"}}");
+        Map<String, String> response = handler.move(moveRequest);
 
-		List<String> options = new ArrayList<String>();
-		options.add("up");
-		options.add("down");
-		options.add("left");
-		options.add("right");
+        List<String> options = new ArrayList<String>();
+        options.add("up");
+        options.add("down");
+        options.add("left");
+        options.add("right");
 
-		assertTrue(options.contains(response.get("move")));
-	}
+        assertTrue(options.contains(response.get("move")));
+    }
 
-	@Test
-	void endTest() throws IOException {
-		JsonNode endRequest = OBJECT_MAPPER.readTree("{}");
-		Map<String, String> response = handler.end(endRequest);
-		assertEquals(0, response.size());
-	}
+    @Test
+    void endTest() throws IOException {
+        JsonNode endRequest = OBJECT_MAPPER.readTree("{}");
+        Map<String, String> response = handler.end(endRequest);
+        assertEquals(0, response.size());
+    }
 
-	@Test
-	void avoidNeckAllTest() throws IOException {
+    @Test
+    void avoidNeckAllTest() throws IOException {
 
-		JsonNode testHead = OBJECT_MAPPER.readTree("{\"x\": 5, \"y\": 5}");
-		JsonNode testBody = OBJECT_MAPPER
-				.readTree("[{\"x\": 5, \"y\": 5}, {\"x\": 5, \"y\": 5}, {\"x\": 5, \"y\": 5}]");
-		ArrayList<String> possibleMoves = new ArrayList<>(Arrays.asList("up", "down", "left", "right"));
-		ArrayList<String> expectedResult = new ArrayList<>(Arrays.asList("up", "down", "left", "right"));
+        JsonNode testHead = OBJECT_MAPPER.readTree("{\"x\": 5, \"y\": 5}");
+        JsonNode testBody = OBJECT_MAPPER
+                .readTree("[{\"x\": 5, \"y\": 5}, {\"x\": 5, \"y\": 5}, {\"x\": 5, \"y\": 5}]");
+        ArrayList<String> possibleMoves = new ArrayList<>(Arrays.asList("up", "down", "left", "right"));
+        ArrayList<String> expectedResult = new ArrayList<>(Arrays.asList("up", "down", "left", "right"));
 
-		handler.avoidMyNeck(testHead, testBody, possibleMoves);
+        handler.avoidMyNeck(testHead, testBody, possibleMoves);
 
-		assertTrue(possibleMoves.size() == 4);
-		assertTrue(possibleMoves.equals(expectedResult));
-	}
+        assertTrue(possibleMoves.size() == 4);
+        assertTrue(possibleMoves.equals(expectedResult));
+    }
 
-	@Test
-	void avoidNeckLeftTest() throws IOException {
+    @Test
+    void avoidNeckLeftTest() throws IOException {
 
-		JsonNode testHead = OBJECT_MAPPER.readTree("{\"x\": 5, \"y\": 5}");
-		JsonNode testBody = OBJECT_MAPPER
-				.readTree("[{\"x\": 5, \"y\": 5}, {\"x\": 4, \"y\": 5}, {\"x\": 3, \"y\": 5}]");
-		ArrayList<String> possibleMoves = new ArrayList<>(Arrays.asList("up", "down", "left", "right"));
-		ArrayList<String> expectedResult = new ArrayList<>(Arrays.asList("up", "down", "right"));
+        JsonNode testHead = OBJECT_MAPPER.readTree("{\"x\": 5, \"y\": 5}");
+        JsonNode testBody = OBJECT_MAPPER
+                .readTree("[{\"x\": 5, \"y\": 5}, {\"x\": 4, \"y\": 5}, {\"x\": 3, \"y\": 5}]");
+        ArrayList<String> possibleMoves = new ArrayList<>(Arrays.asList("up", "down", "left", "right"));
+        ArrayList<String> expectedResult = new ArrayList<>(Arrays.asList("up", "down", "right"));
 
-		handler.avoidMyNeck(testHead, testBody, possibleMoves);
+        handler.avoidMyNeck(testHead, testBody, possibleMoves);
 
-		assertTrue(possibleMoves.size() == 3);
-		assertTrue(possibleMoves.equals(expectedResult));
-	}
+        assertTrue(possibleMoves.size() == 3);
+        assertTrue(possibleMoves.equals(expectedResult));
+    }
 
-	@Test
-	void avoidNeckRightTest() throws IOException {
+    @Test
+    void avoidNeckRightTest() throws IOException {
 
-		JsonNode testHead = OBJECT_MAPPER.readTree("{\"x\": 5, \"y\": 5}");
-		JsonNode testBody = OBJECT_MAPPER
-				.readTree("[{\"x\": 5, \"y\": 5}, {\"x\": 6, \"y\": 5}, {\"x\": 7, \"y\": 5}]");
-		ArrayList<String> possibleMoves = new ArrayList<>(Arrays.asList("up", "down", "left", "right"));
-		ArrayList<String> expectedResult = new ArrayList<>(Arrays.asList("up", "down", "left"));
+        JsonNode testHead = OBJECT_MAPPER.readTree("{\"x\": 5, \"y\": 5}");
+        JsonNode testBody = OBJECT_MAPPER
+                .readTree("[{\"x\": 5, \"y\": 5}, {\"x\": 6, \"y\": 5}, {\"x\": 7, \"y\": 5}]");
+        ArrayList<String> possibleMoves = new ArrayList<>(Arrays.asList("up", "down", "left", "right"));
+        ArrayList<String> expectedResult = new ArrayList<>(Arrays.asList("up", "down", "left"));
 
-		handler.avoidMyNeck(testHead, testBody, possibleMoves);
+        handler.avoidMyNeck(testHead, testBody, possibleMoves);
 
-		assertTrue(possibleMoves.size() == 3);
-		assertTrue(possibleMoves.equals(expectedResult));
-	}
+        assertTrue(possibleMoves.size() == 3);
+        assertTrue(possibleMoves.equals(expectedResult));
+    }
 
-	@Test
-	void avoidNeckUpTest() throws IOException {
+    @Test
+    void avoidNeckUpTest() throws IOException {
 
-		JsonNode testHead = OBJECT_MAPPER.readTree("{\"x\": 5, \"y\": 5}");
-		JsonNode testBody = OBJECT_MAPPER
-				.readTree("[{\"x\": 5, \"y\": 5}, {\"x\": 5, \"y\": 6}, {\"x\": 5, \"y\": 7}]");
-		ArrayList<String> possibleMoves = new ArrayList<>(Arrays.asList("up", "down", "left", "right"));
-		ArrayList<String> expectedResult = new ArrayList<>(Arrays.asList("down", "left", "right"));
+        JsonNode testHead = OBJECT_MAPPER.readTree("{\"x\": 5, \"y\": 5}");
+        JsonNode testBody = OBJECT_MAPPER
+                .readTree("[{\"x\": 5, \"y\": 5}, {\"x\": 5, \"y\": 6}, {\"x\": 5, \"y\": 7}]");
+        ArrayList<String> possibleMoves = new ArrayList<>(Arrays.asList("up", "down", "left", "right"));
+        ArrayList<String> expectedResult = new ArrayList<>(Arrays.asList("down", "left", "right"));
 
-		handler.avoidMyNeck(testHead, testBody, possibleMoves);
+        handler.avoidMyNeck(testHead, testBody, possibleMoves);
 
-		assertTrue(possibleMoves.size() == 3);
-		assertTrue(possibleMoves.equals(expectedResult));
-	}
+        assertTrue(possibleMoves.size() == 3);
+        assertTrue(possibleMoves.equals(expectedResult));
+    }
 
-	@Test
-	void avoidNeckDownTest() throws IOException {
+    @Test
+    void avoidNeckDownTest() throws IOException {
 
-		JsonNode testHead = OBJECT_MAPPER.readTree("{\"x\": 5, \"y\": 5}");
-		JsonNode testBody = OBJECT_MAPPER
-				.readTree("[{\"x\": 5, \"y\": 5}, {\"x\": 5, \"y\": 4}, {\"x\": 5, \"y\": 3}]");
-		ArrayList<String> possibleMoves = new ArrayList<>(Arrays.asList("up", "down", "left", "right"));
-		ArrayList<String> expectedResult = new ArrayList<>(Arrays.asList("up", "left", "right"));
+        JsonNode testHead = OBJECT_MAPPER.readTree("{\"x\": 5, \"y\": 5}");
+        JsonNode testBody = OBJECT_MAPPER
+                .readTree("[{\"x\": 5, \"y\": 5}, {\"x\": 5, \"y\": 4}, {\"x\": 5, \"y\": 3}]");
+        ArrayList<String> possibleMoves = new ArrayList<>(Arrays.asList("up", "down", "left", "right"));
+        ArrayList<String> expectedResult = new ArrayList<>(Arrays.asList("up", "left", "right"));
 
-		handler.avoidMyNeck(testHead, testBody, possibleMoves);
+        handler.avoidMyNeck(testHead, testBody, possibleMoves);
 
-		assertTrue(possibleMoves.size() == 3);
-		assertTrue(possibleMoves.equals(expectedResult));
-	}
+        assertTrue(possibleMoves.size() == 3);
+        assertTrue(possibleMoves.equals(expectedResult));
+    }
 }
